cc47e53d4cf9b682177c5c71d56a177c1f844fb3
Kevin Fredy CALMO <kevinfcalmo@gmail.com>
2022-04-26 21:21:41 -0500

    Début de la partie desktop

--- app.js
+++ app.js
@@ -19,16 +19,23 @@ function navBarFixed(){
 const menu = document.getElementById('menu');
 const close = document.getElementById('close');
 const links = document.getElementById('links');
-    const herobanner_img = document.getElementById('herobanner-img');
+const mobile = document.getElementById('mobile');
+const herobanner_img = document.getElementById('herobanner-img');
+const body = document.getElementById('body');
 
 menu.addEventListener('click', ()=>{
     links.classList.add('visible');
     menu.classList.add('close');
     close.classList.add('visible');
+    mobile.classList.add('visible');
+    body.classList.add('mobile');
+    
 });
 
 close.addEventListener('click', ()=> {
     links.classList.remove('visible');
     menu.classList.remove('close');
     close.classList.remove('visible');
+    mobile.classList.remove('visible');
+    body.classList.remove('mobile');
 })
